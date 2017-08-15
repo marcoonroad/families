56a0493af0969e0e2d72da6183e1a3433f3bc34c
Marco Aurélio da Silva <marcoonroad@gmail.com>
2017-08-14 23:49:47 -0300

    trying to fix travis-ci build

--- travis-ci.sh
+++ travis-ci.sh
@@ -8,18 +8,24 @@ else
     CURRENT_DIRECTORY=`pwd`
     cd $TRAVIS_CACHE_DIRECTORY
 
-    echo "Building lua..."
+    echo ""
+    echo "==========================================================="
+    echo "*** Building lua..."
     curl -R -O http://www.lua.org/ftp/lua-$LUA_VERSION.tar.gz
     tar zxf lua-$LUA_VERSION.tar.gz
     cd lua-$LUA_VERSION
     make linux test
     make local
     cd ..
-    echo "Lua is built!"
+    echo "*** Lua is built!"
+    echo "==========================================================="
+    echo ""
 
     ###########################################################################
 
-    echo "Building luarocks..."
+    echo ""
+    echo "==========================================================="
+    echo "*** Building luarocks..."
     wget https://www.luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz
     tar zxpf luarocks-$LUAROCKS_VERSION.tar.gz
     cd luarocks-$LUAROCKS_VERSION
@@ -28,27 +34,37 @@ else
     make build
     make install
     cd ..
-    echo "Luarocks is built!"
+    echo "*** Luarocks is built!"
+    echo "==========================================================="
+    echo ""
 
     ###########################################################################
 
-    echo "Linking directories..."
+    echo ""
+    echo "==========================================================="
+    echo "*** Linking directories..."
     ln -s lua-$LUA_VERSION/install/bin bin
     ln -s lua-$LUA_VERSION/install/lib lib
     ln -s lua-$LUA_VERSION/install/include include
     ln -s lua-$LUA_VERSION/install/share share
     ln -s lua-$LUA_VERSION/install/man man
-    echo "Linked directories!"
-
-    ###########################################################################
-
-    echo "Setting up environment..."
     cd $CURRENT_DIRECTORY
-    PATH=$TRAVIS_CACHE_DIRECTORY/bin:$PATH
-    export PATH
-    LD_LIBRARY_PATH=$TRAVIS_CACHE_DIRECTORY/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH
-    echo "Configuration is done!"
+    echo "*** Linked directories!"
+    echo "==========================================================="
+    echo ""
 fi
+
+echo ""
+echo "==============================================================="
+echo "*** Setting up environment..."
+PATH=$TRAVIS_CACHE_DIRECTORY/bin:$PATH
+export PATH
+LD_LIBRARY_PATH=$TRAVIS_CACHE_DIRECTORY/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+MANPATH=$TRAVIS_CACHE_DIRECTORY/man:$MANPATH
+export MANPATH
+echo "*** Configuration is done!"
+echo "==============================================================="
+echo ""
 
 # END
